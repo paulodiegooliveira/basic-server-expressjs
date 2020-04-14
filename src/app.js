22970f916cb5e9586aaf6636167f1ab716cc76d5
paulodiegooliveira <paulodiego.oliveira@gmail.com>
2020-04-14 05:09:38 -0300

    [express] Read e Update

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+
+clients = [];
+
+app.get("/clients/", (request, response) => {
+  response.json(clients);
+  return;
+});
+
+app.post("/clients/", (request, response) => {
+  const { client, company } = request.body;
+
+  const costume = {
+    id: uuid(),
+    client,
+    company,
+  };
+
+  clients.push(costume);
+  return response.json(costume);
+});
+
+app.put("/clients/:id", (request, response) => {
+  const { id } = request.params;
+  const { client, company } = request.body;
+
+  const clientsFindIndex = clients.findIndex((param) => param.id === id);
+
+  if (clientsFindIndex < 0) {
+    return response.json({ erro: "ErrEmpty" });
+  }
+
+  const costume = {
+    id,
+    client,
+    company,
+  };
+
+  clients[clientsFindIndex] = costume;
+  // console.log(clients);
+
+  return response.json(costume);
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Server started");
+});
